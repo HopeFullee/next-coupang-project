857293b64cf9af0bb3ec52485e6d2c13903c77d9
HopeFullee <john_lee0607@naver.com>
2023-08-10 23:17:45 +0900

    feat: 수량 기능 추가

--- src/components/products/ProductList/ProductCard/index.tsx
+++ src/components/products/ProductList/ProductCard/index.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useState } from "react";
 import { Product } from "@/types/Product";
 import Image from "next/image";
 
@@ -6,6 +8,16 @@ interface Props extends Product {
 }
 
 const ProductCard = ({ name, stock, price, thumbnail, calcValue }: Props) => {
+  const [quantity, setQuantity] = useState(0);
+
+  const increaseQuantityClick = () => {
+    if (quantity < 30) setQuantity((prev) => prev + 1);
+  };
+
+  const reduceQuantityClick = () => {
+    if (quantity > 0) setQuantity((prev) => prev - 1);
+  };
+
   return (
     <li className="p-20 leading-relaxed rounded-md bg-white/20">
       <h6 className="font-bold text-18 md:text-20">{name}</h6>
@@ -21,9 +33,18 @@ const ProductCard = ({ name, stock, price, thumbnail, calcValue }: Props) => {
       <p className="font-semibold md:text-18">
         price: {price.toLocaleString()}
       </p>
-      <div className="mt-20 gap-15 flex-col-center under:p-6 under:w-full under:rounded-md under:font-bold under:tracking-wide">
-        <button className="bg-indigo-500 text-15 md:text-16">REVIEWS</button>
-        <button className="bg-blue-500 text-15 md:text-16">ADD CART</button>
+      <div className="mt-20 gap-15 flex-col-center under:w-full all:rounded-md under:font-bold under:tracking-wide">
+        <button className="p-6 bg-indigo-500 text-15 md:text-16">
+          REVIEWS
+        </button>
+        <div className="flex gap-10 under:p-6 under:bg-blue-500 text-15 md:text-16 under:w-full">
+          <div className="flex text-center justify-evenly under:w-30">
+            <button onClick={reduceQuantityClick}>-</button>
+            <p>{quantity}</p>
+            <button onClick={increaseQuantityClick}>+</button>
+          </div>
+          <button>ADD CART</button>
+        </div>
       </div>
     </li>
   );
